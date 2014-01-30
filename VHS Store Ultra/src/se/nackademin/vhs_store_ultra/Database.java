8abcda25de0c5b0e0a804ac172e727e1013cebd5
Gustav Malm <soulcabbadge@live.se>
2014-01-30 11:42:21 +0100

    Project VHS Store Ultra Update

--- VHS Store Ultra/src/se/nackademin/vhs_store_ultra/Database.java
+++ VHS Store Ultra/src/se/nackademin/vhs_store_ultra/Database.java
@@ -1,85 +1,187 @@
 package se.nackademin.vhs_store_ultra;
 
-//NiclasBerner IS COMMITING!
-//HEJ!
-
-// 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
-// Vår fina databas :)
 public class Database {
 
 	public static void main(String[] args) {
 		MysqlDataSource ds = new MysqlDataSource();
 		ds.setServerName("localhost");
 		ds.setPort(3306);
-		ds.setDatabaseName("vhs_store_ultra");
-		
+		ds.setDatabaseName("soccer_club");
+
 		Connection con = null;
 		try {
-			// DO NOT SHOW YOUR PASSWORD IN YOUR CODE ON A REAL PROJECT!
-			con = ds.getConnection("root", "");
+			//DO NOT SHOW PASSWORD IN YOUR CODE IN YOUR REAL APPLICATION
+			con = ds.getConnection("tester", "test");
 		} catch (SQLException e) {
-			System.err.println("Error: Couldn't connect " + e.getMessage());
+			System.err.println("Error: Couldn't connect. " + e.getMessage());
 			return;
 		}
-
-		System.out.println("Connection successfull");
+		
+		System.out.println("Connection successful!");
 		
 		Statement queryCaller = null;
 		try {
 			queryCaller = con.createStatement();
 		} catch (SQLException e) {
-			System.err.println("Error: Could not create statement" + e.getMessage());
+			System.err.println("Error! Could not create statement. " + e.getMessage());
 			try {
 				con.close();
 			} catch (SQLException e1) {
-				System.err.println("Error: Could not close connection " + e1.getMessage());
-			
+				System.err.println("Warning: Could not close connection. " + e.getMessage());
 			}
 			return;
 		}
 		
-		System.out.println("Create statement successfull");
+		System.out.println("Create statement successful!");
 		
 		ResultSet results = null;
-		
+				
 		try {
-			results = queryCaller.executeQuery("SELECT * FROM customer");
-			System.out.println("Print first row: ");
+			//INSERT
+//			int affectedRows = queryCaller.executeUpdate("INSERT INTO person VALUES('800312', 'Lionel', '08112233')");
+//			System.out.println("Affected rows: " + affectedRows);
+			
+			//SELECT
+			results = queryCaller.executeQuery("SELECT * FROM person");
+			System.out.println("Print first row:");
 			results.first();
 			System.out.println(results.getString("name"));
-			System.out.println(results.getString("city"));
 			
-			System.out.println("Print all rows: ");
+			System.out.println("Print all rows:");
 			results.beforeFirst();
-			while(results.next()) {
-				
+			while(results.next()){
 				System.out.println(results.getString("name"));
-				System.out.println(results.getString("city"));
-				
+			}
+
+			//MetaData
+			ResultSetMetaData resultInfo = results.getMetaData();
+			int nCols = resultInfo.getColumnCount();
+			for(int i = 1; i < nCols+1; i++){
+				System.out.print(resultInfo.getColumnLabel(i) + " ");
+			}
+			System.out.println();
+			results.beforeFirst();
+			while(results.next()){
+				for(int i = 1; i < nCols+1; i++){
+					System.out.print(results.getString(i) + " ");
+				}
+				System.out.println();
 			}
 			
-		}
-		catch (SQLException e){
+			
+			
+		} catch (SQLException e) {
 			System.err.println("SQL query failed! " + e.getMessage());
 		}
 		
-		// CLOSE EVERYTHING
+		//BATCH
+		try {
+			queryCaller.addBatch("INSERT INTO person VALUES('800313', 'Jennie', '08112233')");
+			queryCaller.addBatch("INSERT INTO person VALUES('800314', 'Raynold', '08112233')");
+			queryCaller.addBatch("INSERT INTO person VALUES('800315', 'Patric', '08112233')");
+			queryCaller.addBatch("DELETE FROM person WHERE personid='800315'");
+			queryCaller.executeBatch();
+			System.out.println("Batching success!");
+		} catch (SQLException e1) {
+			System.err.println("SQL query failed! " + e1.getMessage());
+		}
+		
+		//PREPARED
+		PreparedStatement preparedStm = null;
+		try {
+			String nameInput = "Anna";
+			preparedStm = con.prepareStatement("INSERT INTO person VALUES(?, ?, '08112233')");
+			preparedStm.setInt(1, 871111);
+			preparedStm.setString(2, nameInput);
+			preparedStm.executeUpdate();
+
+			preparedStm.setInt(1, 871114);
+			preparedStm.setString(2, "Charles");
+			preparedStm.addBatch();
+			preparedStm.setInt(1, 871112);
+			preparedStm.addBatch();
+			preparedStm.executeBatch();
+			System.out.println("Prepared statement usage success!");
+			
+		} catch (SQLException e1) {
+			System.err.println("Error! Prepared statement failed. " + e1.getMessage());
+		} finally{
+			//We're only using the preparedStatement object here, so we can close it in the finally-block
+			if(preparedStm != null){
+				try {
+					preparedStm.close();
+				} catch (SQLException e) {
+					System.out.println("Warning: Couldn't close the statement.");
+				}
+			}
+
+		}
+		
+		//TRANSACTION
+		try {
+			con.setAutoCommit(false);
+			//Our new transaction starts----
+			int maxChanges = 5;
+			int nChanges = 0;
+			nChanges += queryCaller.executeUpdate("INSERT INTO person VALUES('901012', 'Yvonne', '08292929')");
+			nChanges += queryCaller.executeUpdate("UPDATE person SET phone='08080808' WHERE name='Yvonne'");
+			nChanges += queryCaller.executeUpdate("INSERT INTO person VALUES('901013', 'Martin', '08292929')");
+			
+			if(nChanges > maxChanges){
+				//Transaction ends. New transaction starts.
+				con.rollback();
+				System.out.println("Too many changes! Rollback. Number of changes: " + nChanges);
+			}
+			else{
+				//Transaction ends. New transaction starts.
+				con.commit();
+				System.out.println("Commiting changes.");
+			}
+		} catch (SQLException e1) {
+			try {
+				//Transaction ends. New transaction starts.
+				con.rollback();
+			} catch (SQLException e) {
+				System.err.println("Rollback failed! " + e1.getMessage());
+			}
+			System.err.println("Transaction failed! " + e1.getMessage());
+		}
+
+		try {
+			//Turning autocommit back on
+			con.setAutoCommit(true);
+		} catch (SQLException e1) {
+			System.err.println("Error! Could not set autocommit to true.");
+		}
+		
+		//CLOSE EVERYTHING
 		if(results != null){
 			try {
 				results.close();
+			} catch (SQLException e) {
+				System.out.println("Warning: Couldn't close results.");
 			}
-			catch(Exception e) {
-				System.out.println("Warning: Couldn't close the statement...");
+		}
+		if(queryCaller != null){
+			try {
+				queryCaller.close();
+			} catch (SQLException e) {
+				System.out.println("Warning: Couldn't close the statement.");
 			}
-			
+		}
+		if(con != null){
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Warning: Couldn't close the connection.");
+			}
 		}
 	}
+	
+	
 
 }
