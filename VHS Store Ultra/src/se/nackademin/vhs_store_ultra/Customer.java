65776b326ea5ca8ff30930a1525060d070b44f43
norman <sebastian.norman@yh.nackademin.se>
2014-02-07 14:41:28 +0100

    Updated UserGUI, added CustomerGUI

--- VHS Store Ultra/src/se/nackademin/vhs_store_ultra/Customer.java
+++ VHS Store Ultra/src/se/nackademin/vhs_store_ultra/Customer.java
@@ -1,7 +1,34 @@
 package se.nackademin.vhs_store_ultra;
 
-public class Customer {
-	//seflgkdfjgkhj
-	
-	//<siogsjfgk.dfjhoifdhjoit
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Customer extends JFrame {
+
+	public static void main(String[] args) {
+		Customer frameTabel = new Customer();
+	}
+
+	JPanel panel = new JPanel();
+
+	public Customer() {
+		super("Welcome");
+		setSize(300, 200);
+		setLocation(500, 280);
+		getContentPane().setLayout(null);
+		panel.setBounds(0, 0, 284, 161);
+		panel.setLayout(null);
+
+		getContentPane().add(panel);
+
+		JButton btnNewButton = new JButton("Klick!");
+		btnNewButton.setBounds(88, 100, 89, 23);
+		panel.add(btnNewButton);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 }
+
+//
--- VHS Store Ultra/src/se/nackademin/vhs_store_ultra/UserGUI.java
+++ VHS Store Ultra/src/se/nackademin/vhs_store_ultra/UserGUI.java
@@ -2,6 +2,7 @@ package se.nackademin.vhs_store_ultra;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 
@@ -19,21 +20,22 @@ import javax.swing.JLabel;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.PublicKey;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 
 public class UserGUI {
 
 	private JFrame frame;
-	private JTextField textF_userN;
-	private JPasswordField passwordField;
-	private JTextField textF_Name;
+	private JTextField txtUserName;
+	private JPasswordField passWField;
+	private JTextField txtMailField;
+	JButton logButton = new JButton("Logga in");
 
 	/**
 	 * Launch the application.
 	 */
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -51,6 +53,8 @@ public class UserGUI {
 	 */
 	public UserGUI() {
 		initialize();
+		// actionLogin();
+
 	}
 
 	/**
@@ -63,93 +67,114 @@ public class UserGUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setTitle("VHS Store Ultra");
+
 		frame.setResizable(false);
-		
-		textF_userN = new JTextField();
-		textF_userN.setColumns(10);
-		textF_userN.setBounds(380, 212, 89, 20);
-		frame.getContentPane().add(textF_userN);
-		
+
+		txtUserName = new JTextField();
+		txtUserName.setColumns(10);
+		txtUserName.setBounds(380, 212, 89, 20);
+		frame.getContentPane().add(txtUserName);
+
 		JTextArea txtrAnvndarnam = new JTextArea();
 		txtrAnvndarnam.setText("Anv\u00E4ndarnamn:");
 		txtrAnvndarnam.setEditable(false);
 		txtrAnvndarnam.setBackground(SystemColor.menu);
 		txtrAnvndarnam.setBounds(266, 210, 108, 22);
 		frame.getContentPane().add(txtrAnvndarnam);
-		
+
 		JTextArea txtrLsenord = new JTextArea();
 		txtrLsenord.setText("L\u00F6senord:");
 		txtrLsenord.setEditable(false);
 		txtrLsenord.setBackground(SystemColor.menu);
 		txtrLsenord.setBounds(298, 234, 76, 22);
 		frame.getContentPane().add(txtrLsenord);
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(380, 236, 89, 20);
-		frame.getContentPane().add(passwordField);
-		
+
+		passWField = new JPasswordField();
+		passWField.setBounds(380, 236, 89, 20);
+		frame.getContentPane().add(passWField);
+
 		JTextArea txtrMedlem = new JTextArea();
 		txtrMedlem.setText("Inte medlem?");
 		txtrMedlem.setEditable(false);
 		txtrMedlem.setBackground(SystemColor.menu);
 		txtrMedlem.setBounds(10, 236, 106, 22);
 		frame.getContentPane().add(txtrMedlem);
-		
-		textF_Name = new JTextField();
-		textF_Name.addMouseListener(new MouseAdapter() {
+
+		txtMailField = new JTextField();
+		txtMailField.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				textF_Name.setText("");
+				txtMailField.setText("");
 
-				
 			}
 		});
-		textF_Name.setText("skriv in din mail");
-		textF_Name.setHorizontalAlignment(SwingConstants.CENTER);
-		textF_Name.setColumns(10);
-		textF_Name.setBounds(10, 262, 112, 23);
-		frame.getContentPane().add(textF_Name);
-		
+		txtMailField.setText("skriv in din mail");
+		txtMailField.setHorizontalAlignment(SwingConstants.CENTER);
+		txtMailField.setColumns(10);
+		txtMailField.setBounds(10, 262, 112, 23);
+		frame.getContentPane().add(txtMailField);
+
+		// /////////////////////////////////////////////////////////////////
+		logButton.setBounds(380, 262, 89, 23);
+		frame.getContentPane().add(logButton);
+
+		JLabel lblNewLabel = new JLabel();
+		lblNewLabel.setLocation(69, 11);
+
+		// JLabel lblNewLabel = new JLabel();
+		// /////////////////////////////////////////////////////////////////
+
+		ImageIcon icon = new ImageIcon("img/vhs_logo.png");
+		lblNewLabel.setIcon(icon);
+
+		// lblNewLabel.setBounds(10, 11, 252, 135);
+		lblNewLabel.setSize(326, 179);
+		frame.getContentPane().add(lblNewLabel);
+
 		JButton ansökButton = new JButton("Ans\u00F6k");
 		ansökButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				/*
-				
-				//Ansök BUTTTONONONONONONONONNOONN!!!!!!!!!!!!!!!!!!!
-				FileWriter pw;
-				try {
-					documentFile = new File("default.txt");
-					pw = new FileWriter ("default.txt", false);
-					textArea.write(pw);
 
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-				
-				*/
+				/*
+				 * 
+				 * //Ansök BUTTTONONONONONONONONNOONN!!!!!!!!!!!!!!!!!!!
+				 * FileWriter pw; try { documentFile = new File("default.txt");
+				 * pw = new FileWriter ("default.txt", false);
+				 * textArea.write(pw);
+				 * 
+				 * } catch (IOException e1) { e1.printStackTrace(); }
+				 */
 			}
 		});
 		ansökButton.setBounds(125, 262, 89, 23);
 		frame.getContentPane().add(ansökButton);
-		
-		JButton button_1 = new JButton("Logga in");
-		button_1.setBounds(380, 262, 89, 23);
-		frame.getContentPane().add(button_1);
-		
-		JLabel lblNewLabel = new JLabel();
-		lblNewLabel.setLocation(69, 11);
-		
-		//JLabel lblNewLabel = new JLabel();
 
-		
-		ImageIcon icon = new ImageIcon("img/vhs_logo.png"); 
-		lblNewLabel.setIcon(icon);
-		
-		//lblNewLabel.setBounds(10, 11, 252, 135);
-		lblNewLabel.setSize(326, 179);
-		frame.getContentPane().add(lblNewLabel);
+		// public void actionLogin(){
+		logButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+				String puname = txtUserName.getText();
+				String ppaswd = passWField.getText();
+				if (puname.equals("123") && ppaswd.equals("123")) {
+					Customer regFace = new Customer();
+					regFace.setVisible(true);
+					frame.dispose();
+				} else {
+
+					JOptionPane.showMessageDialog(null,
+							"Wrong Password / Username");
+					txtMailField.setText("");
+					passWField.setText("");
+					txtMailField.requestFocus();
+				}
+
+			}
+		});
+
 	}
+
 }
+
+// }
 
 //
